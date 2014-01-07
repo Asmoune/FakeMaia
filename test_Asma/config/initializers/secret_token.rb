a33978cfff91d06ee68f2767ff9f92e0c586291a
asma <asma.alibert@gmail.com>
2014-01-07 20:28:57 +0100

    suppression dossier

--- test_Asma/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-TestAsma::Application.config.secret_token = 'c39aa7a5cc83dc7ff462a9a9d0876d9800ed9720a19d30c042f4df9a356880608e6296ae3561a2bc9b4fa2556ffeb946d6945c37e367b2fee9ca68c50123054e'
